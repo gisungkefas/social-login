448989780f14a95df9017d2cd4d3f1d4af3615bf
Decagon <decagon@Decagons-MacBook-Air.local>
2023-02-14 12:21:14 +0100

    Implmented Home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
     <div className="App">
      <Navbar />
+     <Home />
     </div>
     </>
   );
